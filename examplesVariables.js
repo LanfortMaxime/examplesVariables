c08f4a83eb32f57e28b168bd1e38fbf78072bf2e
Maxime Lanfort <91140406+LanfortMaxime@users.noreply.github.com>
2021-12-28 09:51:56 +0100

    Add files via upload

---
+++ examplesVariables.js
@@ -0,0 +1,41 @@
+// LES VARIABLES 
+
+let prenom = "Maxime"
+// // Chaînes de caractères (string)
+
+let age = 31;
+// // Nombre (number)
+
+let majorite = true;
+// // Booleen (boolean)
+
+const prenom = "Maxime";
+
+// const prenom = "Maxime"; // ne peut pas être modifié
+
+var prenom = "Maxime";
+
+console.log(prenom);
+
+// LA CONCATENATION
+
+const prenom  = "Maxime";
+const nom     = "Lanfort";
+
+console.log("Bonjour " + prenom + ' ' + nom + ' !');
+
+// Bonjour [prenom] [age] !
+
+// OPERATEURS
+// - "+" addition
+// - "-" soustraction
+// - "*" multiplication
+// - "/" division
+// - "%" modulo <=> 5/2 = 2.45343 restant 1
+// - "**" exponentielle 5**2 = 25
+
+let nombreUn = 8, nombreDeux = 4, result;
+result = (nombreUn + nombreDeux) * 9; // 108
+result++; // incrémentation = ajoute 1
+result--; // décrémentation = retire 1
+console.log(result);
